ebc872bbd2c3de0085a31423ea704f6ec7952efb
Asishk9 <asishkumargupta98@gmail.com>
2023-08-18 09:04:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 import Contextprovider from "./Components/context/Contextprovider";
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.render(
   <Contextprovider>
